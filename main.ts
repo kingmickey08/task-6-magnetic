42f77f276e744046d13a1b42ffe1fc6f1bcfec90
kingmickey08 <81299672+kingmickey08@users.noreply.github.com>
2022-09-28 15:09:39 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 basic.forever(function () {
+    serial.writeValue("temp", input.temperature())
     serial.writeValue("magnetic", input.magneticForce(Dimension.Y))
     serial.writeValue("rotasi", input.rotation(Rotation.Pitch))
-    serial.writeValue("temp", input.temperature())
     serial.writeValue("light", input.lightLevel())
     serial.writeValue("sound", input.soundLevel())
+    serial.writeValue("compass", input.compassHeading())
 })
